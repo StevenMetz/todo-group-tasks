5cdca951ed0d0e45db1f87f11cab1bd0cd95aa69
StevenMetz <stevenm.codes@gmail.com>
2023-01-02 14:07:07 -0600

    employee can login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  def current_employee
+    auth_headers = request.headers["Authorization"]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: "HS256" }
+        )
+        Employee.find_by(id: decoded_token[0]["employee_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  def authenticate_employee
+    unless current_employee
+      render json: {}, status: :unauthorized
+    end
+  end
+end
 end
