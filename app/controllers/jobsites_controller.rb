e27072e01f956bdf09d16b4bef10e4ee93808433
StevenMetz <stevenm.codes@gmail.com>
2023-01-06 21:28:01 -0600

    jobsites added

--- app/controllers/jobsites_controller.rb
+++ app/controllers/jobsites_controller.rb
@@ -1,16 +1,45 @@
 class JobsitesController < ApplicationController
   def index
+    @jobsites = Jobsite.all
+    render json: @jobsites.as_json
   end
 
   def show
+    @jobsite = Jobsite.find_by(id: params[:id])
+    render json: @jobsite.as_json
   end
 
   def create
+    jobsite = Jobsite.new(
+      employee_id: params[:employee_id],
+      location: params[:location],
+      builder: params[:builder],
+    )
+    if jobsite.save
+      render json: jobsite.as_json
+    else
+      render json: { errors: jobsite.errors.full_message }, status: :bad_request
+    end
   end
 
   def update
+    @jobsite = Jobsite.find_by(id: params[:id])
+    @jobsite.location = params[:location] || @jobsite.location
+    @jobsite.builder = params[:builder] || @jobsite.builder
+    @jobsite.employee_id = params[:employee_id] || @jobsite.employee_id
+    if @jobsite.save
+      render json: @jobsite.as_json
+    else
+      render json: { errors: @jobsite.errors.full_message }, status: :bad_request
+    end
   end
 
   def destroy
+    @jobsite = Jobsite.find_by(id: params[:id])
+    if @jobsite.destroy
+      render json: :jobsite_removed #{ message: "Jobsite Removed" }
+    else
+      render json: { errors: @jobsite.errors.full_message }, status: :bad_request
+    end
   end
 end
