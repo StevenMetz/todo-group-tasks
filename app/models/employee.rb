4e91cca63a5eb16a74f9e1b884dd51dd5d75741c
StevenMetz <stevenm.codes@gmail.com>
2023-01-29 10:23:09 -0600

    employees show up on jobsites and jobsites show up on employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,14 +1,10 @@
 class Employee < ApplicationRecord
-  # has_many :employees, class_name: "Employee", foreign_key: "manager_id"
-  # belongs_to :employee, class_name: "Employee", optional: true
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :todos
   has_many :punchins
-  # has_many :jobsite_employees
   has_many :employee_jobsites
   has_many :jobsites, through: :employee_jobsites
-  # has_and_belongs_to_many :jobsites
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
