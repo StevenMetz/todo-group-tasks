f17c4f69f8fcffb8ad9773e9df5834ac85ca7db1
StevenMetz <stevenm.codes@gmail.com>
2023-02-16 13:29:43 -0600

    cloudinary gem installed for image url uploading

--- Gemfile
+++ Gemfile
@@ -48,3 +48,4 @@ group :development do
 end
 gem 'jwt'
 gem 'jb'
+gem "cloudinary"
--- app/models/employee_jobsite.rb
+++ app/models/employee_jobsite.rb
@@ -1,4 +1,5 @@
 class EmployeeJobsite < ApplicationRecord
   belongs_to :employee
   belongs_to :jobsite
+  has_one_attached :image
 end
